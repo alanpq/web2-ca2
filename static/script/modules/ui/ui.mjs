2d8c26ed4b2204c115a2387d4e422822a4415b01
oofsauce <alanpanayotov@gmail.com>
2022-03-28 00:49:38 +0100

    move ui to class

--- static/script/modules/ui/ui.mjs
+++ static/script/modules/ui/ui.mjs
@@ -22,99 +22,120 @@
  * @prop {number} y Inherited y of the context
  */
 
-/**
- * @type {PositioningContext[]}
+export default class UI {
+  /**
+   * @type {PositioningContext[]}
+   */
+  posStack = [];
+  /** @type {CanvasRenderingContext2D} */
+  ctx;
+
+  /** @type {SidedValues} */
+  textPadding = {top: 2, right: 2, bottom: 2, left: 2};
+
+  /**
+ * @typedef Font
+ * @type {object}
+ * @prop {number} size Font size
+ * @prop {string} unit Font unit (px, em, etc)
+ * @prop {string} family Font family
+ * @prop {string} color Font color
  */
-const posStack = [];
+  /** @type {Font} */
+  font = {
+    size: 12,
+    unit: "px",
+    family: "",
+    color: "white",
+  };
+  get fontString () {
+    return `${font.size}${font.unit} ${font.family}`;
+  }
 
-export const startVertical = () => {
-  const pctx = top();
-  posStack.push({
-    horizontal: false,
-    width: 0,
-    height: 0,
-    x: pctx.x + pctx.width * pctx.horizontal,
-    y: pctx.y + pctx.height * !pctx.horizontal,
-  })
-}
+  constructor(ctx) {
+    this.ctx = ctx;
+  }
 
-export const startHorizontal = () => {
-  const pctx = top();
-  posStack.push({
-    horizontal: true,
-    width: 0,
-    height: 0,
-    x: pctx.x + pctx.width * pctx.horizontal,
-    y: pctx.y + pctx.height * !pctx.horizontal,
-  })
-}
+  /**
+   * 
+   * @param {CanvasRenderingContext2D} ctx 
+   * @param {string} text 
+   */
+  text(text) {
+    //TODO: multiline text
+    this.ctx.fillStyle = "white";
+    this.ctx.font = "20px monospace";
+    const pctx = this.top();
+    const metrics = this.ctx.measureText(text);
+    const w = metrics.width + textPadding.left + textPadding.right;
+    const h = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
+              + textPadding.top + textPadding.bottom;
+    if(pctx.horizontal) { // horizontal flow
+      this.ctx.fillText(text, pctx.x + pctx.width + textPadding.left, pctx.y + textPadding.top);
+      pctx.width += w;
+      pctx.height = Math.max(pctx.height, h);
+    } else { // vertical flow
+      if(pctx.height == 0) pctx.height = h;
+      this.ctx.fillText(text, pctx.x + textPadding.left, pctx.y + pctx.height + textPadding.top);
+      pctx.width = Math.max(pctx.width, w);
+      pctx.height += h;
+    }
+  }
 
-/**
- * Get top of the positioning stack.
- * @returns {PositioningContext}
- */
-const top = () => {
-  if(posStack.length == 0) {
-    return {
+  startVertical() {
+    const pctx = top();
+    posStack.push({
       horizontal: false,
       width: 0,
       height: 0,
-      x: 0,
-      y: 0,
+      x: pctx.x + pctx.width * pctx.horizontal,
+      y: pctx.y + pctx.height * !pctx.horizontal,
+    })
+  }
+
+  startHorizontal() {
+    const pctx = top();
+    posStack.push({
+      horizontal: true,
+      width: 0,
+      height: 0,
+      x: pctx.x + pctx.width * pctx.horizontal,
+      y: pctx.y + pctx.height * !pctx.horizontal,
+    })
+  }
+  // TODO: max depth on queue to prevent memory leak if you forget an end call
+  endVertical() {
+    const old = posStack.pop();
+    if(old.horizontal)
+      throw new Error("Unmatched horizontal flow! Tried to end vertical when a horizontal has not yet been closed.")
+    const n = top();
+    n.x += old.width;
+    n.y += old.height;
+  }
+  
+  endHorizontal() {
+    const old = posStack.pop();
+    if(!old.horizontal)
+      throw new Error("Unmatched vertical flow! Tried to end horizontal when a vertical has not yet been closed.")
+    const n = top();
+    n.height += old.height;
+  }
+
+  /**
+   * Get top of the positioning stack.
+   * @returns {PositioningContext}
+   */
+  top() {
+    if(posStack.length == 0) {
+      return {
+        horizontal: false,
+        width: 0,
+        height: 0,
+        x: 0,
+        y: 0,
+      }
     }
-  }
-  return posStack[posStack.length-1];
-}
-
-/** @type {SidedValues} */
-export const textPadding = {top: 2, right: 2, bottom: 2, left: 2};
-
-export const setTextPadding = (top, right, bottom, left) => {
-  textPadding.top = top || textPadding.top;
-  textPadding.right = right || textPadding.right;
-  textPadding.bottom = bottom || textPadding.bottom;
-  textPadding.left = left || textPadding.left;
-}
-
-/**
- * 
- * @param {CanvasRenderingContext2D} ctx 
- * @param {string} text 
- */
-export const text = (ctx, text) => {
-  //TODO: multiline text
-  const pctx = top();
-  const metrics = ctx.measureText(text);
-  const w = metrics.width + textPadding.left + textPadding.right;
-  const h = metrics.actualBoundingBoxAscent + metrics.actualBoundingBoxDescent
-            + textPadding.top + textPadding.bottom;
-  if(pctx.horizontal) { // horizontal flow
-    ctx.fillText(text, pctx.x + pctx.width + textPadding.left, pctx.y + textPadding.top);
-    pctx.width += w;
-    pctx.height = Math.max(pctx.height, h);
-  } else { // vertical flow
-    if(pctx.height == 0) pctx.height = h;
-    ctx.fillText(text, pctx.x + textPadding.left, pctx.y + pctx.height + textPadding.top);
-    pctx.width = Math.max(pctx.width, w);
-    pctx.height += h;
+    return posStack[posStack.length-1];
   }
 }
 
-//TODO: need to sanity check endVertical/Horizontal, to ensure start's and end's are paired up correctly
-
-export const endVertical = () => {
-  const old = posStack.pop();
-  if(old.horizontal)
-    throw new Error("Unmatched horizontal flow! Tried to end vertical when a horizontal has not yet been closed.")
-  const n = top();
-  n.x += old.width;
-  n.y += old.height;
-}
-
-export const endHorizontal = () => {
-  const old = posStack.pop();
-  if(!old.horizontal)
-    throw new Error("Unmatched vertical flow! Tried to end horizontal when a vertical has not yet been closed.")
-  const n = top();
-  n.height += old.height;
-}
