2652ac99e070d3a92c91c40195048ad0c0d0781a
oofsauce <alanpanayotov@gmail.com>
2022-03-27 19:02:32 +0100

    basic flask app

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask_session import Session
+from flask import Flask, redirect, render_template, request, session, url_for
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "supersecretkey"
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+Session(app)
